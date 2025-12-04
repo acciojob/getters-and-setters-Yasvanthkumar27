fa9d733045cdbba4d951b7cb40a0588212c603d6
Yasvanth Kumar <yasvanthkumar48@gmail.com>
2025-12-04 12:46:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,39 +1,46 @@
-// Person class
+// Base Person class
 class Person {
   constructor(name, age) {
     this._name = name;
     this._age = age;
   }
 
-  // getter & setter for name
+  // getter for name
   get name() {
     return this._name;
   }
 
+  // optional setter for name (not strictly needed by tests)
   set name(newName) {
     this._name = newName;
   }
 
-  // getter & setter for age
+  // getter for age
   get age() {
     return this._age;
   }
 
+  // setter for age
   set age(newAge) {
     this._age = newAge;
   }
 }
 
-// Student class inheriting from Person
+// Student class extending Person
 class Student extends Person {
   study() {
     console.log(${this.name} is studying);
   }
 }
 
-// Teacher class inheriting from Person
+// Teacher class extending Person
 class Teacher extends Person {
   teach() {
     console.log(${this.name} is teaching);
-  }
+  }
 }
+
+// Make classes available on window for Cypress
+window.Person = Person;
+window.Student = Student;
+window.Teacher = Teacher;
